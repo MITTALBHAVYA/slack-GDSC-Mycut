8acb07317be02714f5f5a3b8379de7f261113237
MITTALBHAVYA <bhavya12mittal@gmail.com>
2025-02-15 14:47:59 +0530

    adding url acceptibility in postgre

--- backend/config/databases/postgreconn.js
+++ backend/config/databases/postgreconn.js
@@ -6,11 +6,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const postgresConnection = new Sequelize(
-  process.env.PG_DB,
-  process.env.PG_USER,
-  process.env.PG_PASSWORD,
-  {
+  process.env.PG_URL || process.env.PG_DB,
+  process.env.PG_URL
+  ? {
+    dialect: 'postgres',
+    dialectOptions:{
+      ssl : {
+        require:true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging:false,
+  }
+  :{
     host: process.env.PG_HOST,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
     dialect: 'postgres',
     logging: false,
   }
